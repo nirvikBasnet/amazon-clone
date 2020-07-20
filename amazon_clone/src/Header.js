7ea50774b8995298b26a394c65944ccb75bc2901
nirvikBasnet <nirvik41@gmail.com>
2020-07-20 11:04:02 +1000

    Initial Commit, NavBar(In Progress), React Route

---
+++ amazon_clone/src/Header.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import SearchIcon from '@material-ui/icons/Search';
+import "./Header.css";
+
+function Header() {
+    return (
+        <nav className="header">
+            {/* Logo on the left -> image */}
+            <Link to='/'>
+            <img className="header_logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"/>
+            </Link>
+            
+
+
+            {/* Search Box */}
+            <div className="header__search">
+            <input type='text' className="header__searchInput"></input>
+            <SearchIcon className="header__searchIcon"/>
+            </div>
+
+
+            {/* 3 links */}
+
+            <div className="HeaderNav">
+
+                {/* 1st Link */}
+                <Link to='/login' className="header__link">
+                <div className="header__option">
+                <span className="header__optionLineOne">Hello</span>
+                <span className="header__optionLineTwo">Sign In</span>
+                </div>
+                </Link>
+
+
+                {/* second option */}
+                <Link to='/login' className="header__link">
+                <div className="header__option">
+                <span className="header__optionLineOne">Returns</span>
+                <span className="header__optionLineTwo">& Orders</span>
+                </div>
+                </Link>
+              
+
+
+                {/* third option */}
+                <Link to='/login' className="header__link">
+                <div className="header__option">
+                <span className="header__optionLineOne">Your</span>
+                <span className="header__optionLineTwo">Prime</span>
+                </div>
+                </Link>
+
+
+            </div>
+
+            {/* Basket Icon */}
+        </nav>
+    )
+}
+
+export default Header
